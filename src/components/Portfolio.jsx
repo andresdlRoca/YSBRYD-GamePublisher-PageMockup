f4957690739ce8450f01131c8a1db62ff950d619
andresdlRoca <andresdelaroca11@gmail.com>
2022-04-24 16:35:33 -0600

    Functional plataform selection in portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,6 +14,13 @@ export default function Portfolio() {
     const [clickedGame, setGame] = useState("");
     const [clickedPlatform, setPlatform] = useState("all");
 
+    const vallhallaplats = ["all", "linux", "mac", "pc", "ps4", "switch"];
+    const masqueradaplats = ["all", "linux", "mac", "pc", "ps4", "switch", "xboxone"];
+    const srcplats = ["all", "ios", "mac", "pc", "switch"];
+    const yiikplats = ["all", "linux", "mac", "pc", "psvita", "ps4", "switch"];
+    const wohplats = ["all", "mac", "pc", "ps4", "switch"];
+    const nirvannaplats = ["all", "mac", "pc", "ps4", "switch"];
+    const noplaceplats = ["all", "consoles", "desktop", "pc", "switch"];
 
     const changePlatform = (platform) => {
         setPlatform(platform);
@@ -27,6 +34,7 @@ export default function Portfolio() {
     useEffect(() => {
         console.log(`plataform es ${clickedPlatform}`);
         console.log(`el juego es ${clickedGame}`);
+        console.log(vallhallaplats.includes(clickedPlatform));
     }, [clickedPlatform, clickedGame]);
 
 
@@ -53,49 +61,49 @@ export default function Portfolio() {
             </div>
             <div className="gamegrid">
                 <ul className="gamelist">
-                    <li onClick={() => changeGame("vallhalla")} className="vallhallathumb">
+                    <li onClick={() => changeGame("vallhalla")} className={vallhallaplats.includes(clickedPlatform) ?  "thumbnail visible" : "nonvisible"}>
                         <div className="overlay">
                             <p className="title">VA-11 HALL-A</p>
                             <p className="platforms">LINUX, MAC, PC, PS4, SWITCH</p>
                         </div>
                         <img className="imgthumb" id="vallhalla" src={vallhallathumb} alt="VA-11 HALL-A"/>
                     </li>
-                    <li onClick={() => changeGame("masquerada")} className="masqueradathumb">
+                    <li onClick={() => changeGame("masquerada")} className={masqueradaplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">MASQUERADA: SONGS AND SHADOWS</p>
                             <p className="platforms">LINUX, MAC, PC, PS4, SWITCH, XBOX ONE</p>
                         </div>
                         <img className="imgthumb" id="masquerada" src={masqueradathumb} alt="MASQUERADA: SONGS AND SHADOWS"/>
                     </li>
-                    <li onClick={() => changeGame("sheremebered")} className="srcthumb">
+                    <li onClick={() => changeGame("sheremebered")} className={srcplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">SHE REMEMBERED CATERPILLARS</p>
                             <p className="platforms">IOS, MAC, PC, SWITCH</p>
                         </div>
                         <img className="imgthumb" id="src" src={SRCthumb} alt="SHE REMEMBERED CATERPILLARS"/>
                     </li>
-                    <li onClick={() => changeGame("yiik")} className="yiikthumb">
+                    <li onClick={() => changeGame("yiik")} className={yiikplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">YIIK: A POST-MODERN RPG</p>
                             <p className="platforms">LINUX, MAC, PC, VISTA, PS4, SWITCH</p>
                         </div>
                         <img className="imgthumb" id="yiik" src={yiikthumb} alt="YIIK: A POST MODERN RPG"/>
                     </li>
-                    <li onClick={() => changeGame("woh")} className="worldofhorrorthumb">
+                    <li onClick={() => changeGame("woh")} className={wohplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">WORLD OF HORROR</p>
                             <p className="platforms">MAC, PC, PS4, SWITCH</p>
                         </div>
                         <img className="imgthumb" id="WOH" src={WOHthumb} alt="WORLD OF HORROR"/>
                     </li>
-                    <li onClick={() => changeGame("nirvanna")} className="nirvannathumb">
+                    <li onClick={() => changeGame("nirvanna")} className={nirvannaplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">N1RV ANN-A: CYBERPUNK BARTENDER ACTION</p>
                             <p className="platforms">MAC, PC, PS4, SWITCH</p>
                         </div>
                         <img className="imgthumb" id="nirvanna" src={nirvannathumb} alt="N1RV ANN-A"/>
                     </li>
-                    <li onClick={() => changeGame("noplace")} className="noplacethumb">
+                    <li onClick={() => changeGame("noplace")} className={noplaceplats.includes(clickedPlatform) ?  "thumbnail visible" : " nonvisible"}>
                         <div className="overlay">
                             <p className="title">NO PLACE FOR BRAVERY</p>
                             <p className="platforms">CONSOLES, DESKTOP, PC, SWITCH</p>
